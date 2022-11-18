d61429dd1644eb5ff01e92ebdab2368467e39375
Kate <Kate@mail.com>
2022-11-18 17:27:01 +0800

    20221118

--- TodoApi_Controller/Program.cs
+++ TodoApi_Controller/Program.cs
@@ -5,14 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers();  // 1
 
 builder.Services.AddDbContext<TodoContext>(opt =>  //<- 後加 (見最上方兩行)
     opt.UseInMemoryDatabase("TodoList"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen(); // 2
 
 var app = builder.Build();   //Core的做法，就是都先引入後再一起build
 //這樣就可以DI，保證下方所有的拿到的物件都是同一。
@@ -20,15 +20,15 @@ var app = builder.Build();   //Core的做法，就是都先引入後再一起bui
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger(); // 2
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection();  
 
 app.UseAuthorization();
 
-app.MapControllers(); // <-- 詳情可見︰
+app.MapControllers(); // 1 <-- 詳情可見︰
 //https://learn.microsoft.com/zh-tw/aspnet/core/mvc/controllers/routing?view=aspnetcore-7.0#attribute-routing-for-rest-apis
 
 app.Run();
